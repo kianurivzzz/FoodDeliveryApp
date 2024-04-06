879b17a04bc7f92f6ac8d9a26492e89031df764b
Nikita Karasyov <nikitakarasev177@gmail.com>
2024-04-06 17:23:49 +0300

    Initial Commit

---
+++ FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Никита Карасев on 06.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
